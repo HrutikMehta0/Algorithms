e4ced2f4e93a3c154b45c938ef687961845b2137
Hrutik Mehta <hrutikmehta@icloud.com>
2023-01-18 14:14:32 -0500

    Finished 5_LongestPalindromicSubstring.py 🎉

--- LeetCode/4_MedianOfTwoSortedArray.py
+++ LeetCode/4_MedianOfTwoSortedArray.py
@@ -8,28 +8,26 @@ def findMedianSortedArrays(nums1, nums2):
     :rtype: float
     """
     arr = []
-    i = j = k =0
-    while i<len(nums1) and j < len(nums2):
+    i = j = k = 0
+    while i < len(nums1) and j < len(nums2):
         if nums1[i] < nums2[j]:
             arr.append(nums1[i])
             i += 1
         else:
             arr.append(nums2[j])
-            j+=1
+            j += 1
 
     while i < len(nums1):
         arr.append(nums1[i])
-        i+=1
+        i += 1
     while j < len(nums2):
         arr.append(nums2[j])
-        j+=1
+        j += 1
     if len(arr) % 2 == 0:
-        median = (arr[(len(arr)//2)] + arr[(len(arr)//2)-1])/2
+        median = (arr[(len(arr) // 2)] + arr[(len(arr) // 2) - 1]) / 2
     else:
-        median = arr[(len(arr)//2)]
+        median = arr[(len(arr) // 2)]
     return median
 
 
-
-
-print(findMedianSortedArrays([1, 2], [3,4]))
+print(findMedianSortedArrays([1, 2], [3, 4]))
--- LeetCode/5_LongestPalindromicSubstring.py
+++ LeetCode/5_LongestPalindromicSubstring.py
@@ -4,16 +4,30 @@ def longestPalindrome(s):
     :rtype: str
     """
     longestPal = ''
-    testVal = ''
+    start = 0
+    end = 0
+    if s is None or len(s) < 1:
+        return longestPal
     for i in range(len(s)):
-        testVal += s[i]
-        if testVal == testVal[::-1] and len(testVal) > len(longestPal):
-            longestPal = testVal
-        elif testVal != testVal[::-1]:
-            testVal = ''
+        len1 = expandMiddle(s, i, i)
+        len2 = expandMiddle(s, i, i + 1)
+        lenMax = max(len1, len2)
+        if lenMax > end - start:
+            start = i - (lenMax - 1) // 2
+            end = i + lenMax // 2
+    return s[start:end + 1]
+
+
+def expandMiddle(s, left, right):
+    if s is None or left > right:
+        return 0
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+    return right - left - 1
 
 
 # Test cases
-print(longestPalindrome("babad"))
+print(longestPalindrome("babad"))  # b ba bab
 print(longestPalindrome("cbbd"))
 print(longestPalindrome("a"))
