bee692913955ccbd76444fc251df04506270b1eb
Hrutik Mehta <hrutikmehta@gmail.com>
2023-07-24 23:13:15 -0400

    Working on 11_ContainerWithMostWater.py

--- LeetCode/11_ContainerWithMostWater.py
+++ LeetCode/11_ContainerWithMostWater.py
@@ -1,8 +1,33 @@
-def maxArea(height):
-    pointerOne =0
-    pointerTwo = len(height)-1
+def maxNaiveArea(heights):
+    maxArea = -999999999
+    for i in range(0, len(heights)):
+        for j in range(i, len(heights)):
+            if heights[i] > heights[j]:
+                currArea = (j - i) * heights[j]
+            else:
+                currArea = (j - i) * heights[i]
+        if currArea > maxArea:
+            maxArea = currArea
 
+    return maxArea
+
+
+def maxArea(heights):
+    pointer_one = 0
+    pointer_two = len(heights) - 1
+    maximumArea = -99999999
+    while pointer_two != 0 or pointer_one != len(heights)-1:
+        if heights[pointer_one] > heights[pointer_two]:
+            currArea = (pointer_two - pointer_one) * heights[pointer_two]
+        else:
+            currArea = (pointer_two - pointer_one) * heights[pointer_one]
+        if maximumArea < currArea:
+            maximumArea = currArea
+        pointer_one += 1
+        pointer_two -= 1
+
+    return maximumArea
 
 
 height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
-maxArea(height)
+print(maxArea(height))
--- LeetCode/12_IntegerToRoman.py
+++ LeetCode/12_IntegerToRoman.py
@@ -1,5 +1,14 @@
 def intToRoman(num):
-    if num < 1 or num > 3999:
-        return ""
-    roman = ""
+    romanDict = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
+    result = ""
+    while num > 0:
+
+
+
+
+
+### Test ###
+print(intToRoman(58))
+
+
 
